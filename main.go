7ef7f2aaf81296e4257de77a2193d53ba62345db
brett <brett@brett.local>
2021-08-09 21:26:41 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"compress/flate"
+	"log"
+	"net/http"
+
+	"github.com/bdemetris/tcexec/features/todo"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
+
+func Routes() *chi.Mux {
+	router := chi.NewRouter()
+	compressor := middleware.NewCompressor(flate.DefaultCompression)
+	router.Use(
+		render.SetContentType(render.ContentTypeJSON), // Set content-Type headers as application/json
+		middleware.Logger,          // Log API request calls
+		middleware.RedirectSlashes, // Redirect slashes to no slash URL versions
+		middleware.Recoverer,       // Recover from panics without crashing server
+		compressor.Handler,         // Compress results, mostly gzipping assets and json
+	)
+
+	router.Route("/v1", func(r chi.Router) {
+		r.Mount("/api/todo", todo.Routes())
+	})
+
+	return router
+}
+
+func main() {
+	router := Routes()
+
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route) // Walk and print out all routes
+		return nil
+	}
+	if err := chi.Walk(router, walkFunc); err != nil {
+		log.Panicf("Logging err: %s\n", err.Error()) // panic if there is an error
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", router)) // Note, the port is usually gotten from the environment.
+}
